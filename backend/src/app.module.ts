63a14317c51f77b6fb61c998e6724be96d23ca7f
wali55 <walisantunu@gmail.com>
2026-01-27 16:09:19 +0600

    new

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const port = process.env.PORT || 3000;
-  
-  // IMPORTANT: Bind to '0.0.0.0' so Railway's proxy can find the process
-  await app.listen(port, '0.0.0.0'); 
-  
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
